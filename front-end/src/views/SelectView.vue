cc43cbd10cb5c35f2c3e49487ac5e3ff761ec760
kmkinney <65429603+kmkinney@users.noreply.github.com>
2022-04-13 23:10:53 -0600

    style selection links

--- front-end/src/views/SelectView.vue
+++ front-end/src/views/SelectView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="select">
       <nav-header/>
-      <div v-for="chart in charts" :key="chart._id">
+      <h2>Chore Charts</h2>
+      <div class="chart-links" v-for="chart in charts" :key="chart._id">
           <router-link class="chart-link" :to="{name: 'chart', params: {id: chart._id}}">{{chart.name}}</router-link>
       </div>
   </div>
@@ -41,5 +42,27 @@ export default {
 <style scoped>
 .select {
     color: var(--green);
+}
+.chart-links {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+}
+
+.chart-link {
+    display: block;
+    width: 50%;
+    color: var(--white);
+    background-color: var(--blue);
+    padding: 10px 10px;
+    margin: 20px 0;
+    text-decoration: none;
+    border-radius: 7px;
+}
+
+.chart-link:hover {
+    background-color: var(--green);
 }
 </style>
